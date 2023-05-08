c88a532244cfef362a4e6e2b43cfc6708a34efd8
Dave Chen <totorochina@hotmail.com>
2023-05-08 19:52:20 +0800

    .

--- examples/sd-webui/user-watch.py
+++ examples/sd-webui/user-watch.py
@@ -23,6 +23,7 @@ for line in r.iter_lines(decode_unicode=True):
         if "user" in response['result']['object_meta']['labels']:
             userid = response['result']['object_meta']['labels']['user']
             print(userid)
+            # setup folders here
             if os.path.isdir('/stable-diffusion-webui/outputs'):
                 shutil.rmtree('/stable-diffusion-webui/outputs')
             if os.path.isdir('/stable-diffusion-webui/models'):
@@ -34,4 +35,22 @@ for line in r.iter_lines(decode_unicode=True):
             os.symlink(os.path.join(mount_dir, userid, 'outputs'), '/stable-diffusion-webui/outputs', target_is_directory = True)
             os.symlink(os.path.join(mount_dir, userid, 'inputs'), '/stable-diffusion-webui/inputs', target_is_directory = True)
             os.symlink(os.path.join(mount_dir, 'models'), '/stable-diffusion-webui/models', target_is_directory = True)
+            
+            # webui config files
+            Path(os.path.join(mount_dir, userid, 'configs')).mkdir(parents=True, exist_ok=True)
+            config_files_to_link = ['ui-config.json', 'config.json']
+
+            for file in config_files_to_link:
+                file_origin = os.path.join('/stable-diffusion-webui', file)
+                file_to_link = os.path.join(mount_dir, userid, 'configs', file)
+
+                if os.path.isfile(file_to_link):
+                    if os.path.isfile(file_origin):
+                        shutil.rmtree(file_origin)
+                    os.symlink(file_to_link, file_origin, target_is_directory = False)
+                elif os.path.isfile(file_origin):
+                    shutil.move(file_origin, file_to_link)
+                    os.symlink(file_to_link, file_origin, target_is_directory = False)
+                else:
+                    pass
             break

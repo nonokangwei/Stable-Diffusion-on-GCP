a5ca4c41c488ac41ed808a115a32e346bbe593ad
Dave Chen <totorochina@hotmail.com>
2023-05-20 17:42:28 +0800

    .

--- examples/sd-webui/prelogin.py
+++ examples/sd-webui/prelogin.py
@@ -11,3 +11,8 @@ if os.path.isdir('/stable-diffusion-webui/models'):
     shutil.rmtree('/stable-diffusion-webui/models')
 
 os.symlink(os.path.join(mount_dir, 'models'), '/stable-diffusion-webui/models', target_is_directory = True)
+
+if os.path.isdir('/stable-diffusion-webui/embeddings'):
+    shutil.rmtree('/stable-diffusion-webui/embeddings')
+
+os.symlink(os.path.join(mount_dir, 'embeddings'), '/stable-diffusion-webui/embeddings', target_is_directory = True)

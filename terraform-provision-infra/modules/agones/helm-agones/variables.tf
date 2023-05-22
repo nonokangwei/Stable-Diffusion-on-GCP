4b9d83d41453771183c7f089af2ea711f813ec95
i-jw <forverwj@hotmail.com>
2023-05-22 10:19:44 +0000

    update agones related module

---
+++ terraform-provision-infra/modules/agones/helm-agones/variables.tf
@@ -0,0 +1,16 @@
+variable "gke_cluster_name" {
+  description = "GKE Cluster Name."
+  type        = string
+}
+variable "gke_cluster_location" {
+  description = "GKE Cluster Location"
+  type        = string
+}
+variable "project_id" {
+  description = "GCP project id"
+  type        = string
+}
+variable "gke_cluster_nodepool" {
+  description = "GCP project id"
+  type        = string
+}

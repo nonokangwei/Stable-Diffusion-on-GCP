0b1b2f79c56901b068f039d72d069dca6c0e5020
ChaosEternal <chaoseternal@gmail.com>
2023-07-06 14:15:29 +1000

    Delete Stable-Diffusion-UI-Agones/new-approach directory

--- Stable-Diffusion-UI-Agones/new-approach/make-image.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-. versions.sh
-cd pyapp
-docker build . -t $REG/py-gpu-sche:$TAG
-docker push $REG/py-gpu-sche:$TAG
-cd -
--- Stable-Diffusion-UI-Agones/new-approach/pyapp/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /app
-#mv kubectl /usr/bin/kubectl
-#chmod +x /usr/bin/kubectl
-gunicorn -t 0 --workers=5 alloc_rest:app -b 0:8080
